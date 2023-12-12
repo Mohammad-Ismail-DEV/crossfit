41aa0a51ace144d5294621c550f03a3771096328
ahmad <ahmad.hammoud.1999@hotmail.com>
2023-12-13 00:11:36 +0200

    ContactUs js-css + img

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,7 +1,64 @@
 import React from "react"
+import './ContactUs.css'
+import contactUs from "../../assets/images/ContactUs.jpg"
 
 const ContactUs = () => {
-	return <div>ContactUs</div>
+	return (
+		<div className="fullBody">
+			
+			<div className="form">
+						<div className="input-holder">
+							<div className="placeholder">Name</div>
+							<input
+								className="input"
+								type="text"
+								placeholder="Name"
+							/>
+						</div>
+						<div className="input-holder">
+							<div className="placeholder">Phone Number</div>
+							<input
+								className="input"
+								type="text"
+								placeholder="Phone Number"
+							/>
+						</div>
+						<div className="input-holder">
+							<div className="placeholder">Email</div>
+							<input
+								className="input"
+								type="email"
+								placeholder="example@hotmail.com"
+							/>
+						</div>
+						<div className="input-holder">
+							<div className="placeholder">Subject</div>
+							<input
+								className="input"
+								type="text"
+								placeholder="Subject"
+							/>
+						</div>
+						<div className="input-holder">
+							<div className="placeholder">Message</div>
+							<input
+								className="input"
+								type="longtext"
+								placeholder="Message"
+							/>
+						</div>
+						<div className="submit">
+							Submit
+						</div>
+			</div>
+			<div className="gymImage">
+				<img
+        			src={contactUs}
+        			alt="Contact Us"
+      			/>
+    		</div>
+		</div>
+	);
 }
 
 export default ContactUs
