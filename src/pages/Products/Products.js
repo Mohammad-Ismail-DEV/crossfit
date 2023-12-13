21b8d4ea5398749e3548889115797bca916bd20f
ahmad <ahmad.hammoud.1999@hotmail.com>
2023-12-13 10:59:33 +0200

    Product Page

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,7 +1,24 @@
 import React from "react"
+import './Products.css'
+import productImg from '../../assets/images/product-bg.jpg'
+import userImg from '../../assets/images/user.jpg'
 
 const Products = () => {
-	return <div>Products</div>
+	return (
+	<div>
+		<div className="headImg">
+			<img src={ productImg }/>
+		</div>
+		<div>
+		<div className="card">
+        <img src={userImg} alt="Product Image" style={{ width: '100%' }} />
+        <h1>Product Name</h1>
+        <p className="price">Price</p>
+				 <p>Description of the product</p>
+      </div>
+		</div>
+</div>
+	);
 }
 
 export default Products
